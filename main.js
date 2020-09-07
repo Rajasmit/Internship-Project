6c3a899dc7b26307e537ca386f857229f4c30b5e
Rajasmit <rajasmitmondal@yahoo.com>
2020-09-08 00:57:46 +0530

    Initial Commit of Internship

---
+++ main.js
@@ -0,0 +1,27 @@
+$(window).scroll(function () {
+  var top = $(window).scrollTop();
+  if (top >= 60) {
+    $("nav").addClass("secondary");
+  } else if ($("nav").hasClass("secondary")) {
+    $("nav").removeClass("secondary");
+  }
+});
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: true,
+  autoplayTimeout: 2500,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
